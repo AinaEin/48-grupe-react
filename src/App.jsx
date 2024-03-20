12e96fd27b864f2a58cdd18656c63bf487377489
Aina <ainaeinikyte@gmail.com>
2024-03-20 09:53:17 +0200

    feat: Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Header } from './components/Header';
+import { Hero } from './components/Hero';
+import { Footer } from './components/Footer';
+
+function App() {
+  return (
+  <div>
+  <Header />
+  <Hero />
+    <div>CONTENT 1</div>
+    <div>CONTENT 2</div>
+    <div>CONTENT 3</div>
+    <div>CONTENT 4</div>
+    
+    <Footer />
+    <Footer></Footer>
+  </div>
+  );
+}
+
+export default App;
